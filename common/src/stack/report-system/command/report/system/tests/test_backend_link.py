cfb78311a7d55d3478bc39805f31fa93c46337c5
bgreenb <bsgreenb@eng.ucsd.edu>
2019-04-26 16:45:27 -0700

    WIP: Test now fails if host cannot be ssh into

--- common/src/stack/report-system/command/report/system/tests/test_backend_link.py
+++ common/src/stack/report-system/command/report/system/tests/test_backend_link.py
@@ -8,38 +8,36 @@ from stack import api
 )
 class TestLinkUp:
 
-	# See if the interfaces stacki configures are actually up on the hosts
+	""" See if the interfaces stacki configures are actually up on the hosts """
 
-		def test_link_status(self, backend):
-			errors = []
-			backend_ssh = paramiko.SSHClient()
-			backend_ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+	def test_link_status(self, backend):
+		errors = []
+		backend_ssh = paramiko.SSHClient()
+		backend_ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-			# Get all interfaces for this host
-			interfaces = [
-				host['interface'] for host in api.Call('list host interface ', args=['a:backend']) if host['host'] == backend
-			]
+		# Get all interfaces for this host
+		interfaces = [host['interface'] for host in api.Call(f'list host interface {backend}')]
 
-			try:
-				backend_ssh.connect(hostname=f'{backend}', timeout=5)
+		try:
+			backend_ssh.connect(hostname=f'{backend}', timeout=5)
 
-			except (paramiko.SSHException, socket.error):
-				pytest.skip(f'Could not ssh into host {backend}')
+		except (paramiko.SSHException, socket.error):
+			pytest.fail(f'Could not ssh into host {backend}')
 
-			for link in interfaces:
+		for link in interfaces:
 
-				# Run ethtool on the interface
-				stdin, stdout, stderr = backend_ssh.exec_command(f'ethtool {link}')
-				interface_data = stdout.readlines()
+			# Run ethtool on the interface
+			stdin, stdout, stderr = backend_ssh.exec_command(f'ethtool {link}')
+			interface_data = stdout.readlines()
 
-				# Flag which is set to true if ethtool reports the link is up
-				link_up = False
+			# Flag which is set to true if ethtool reports the link is up
+			link_up = False
 
-				for line in interface_data:
-					if 'Link detected: yes' in line.rstrip():
-						link_up = True
+			for line in interface_data:
+				if 'link detected: yes' in line.lower():
+					link_up = True
 
-				if not link_up:
-					errors.append(link)
+			if not link_up:
+				errors.append(link)
 
-			assert not errors, f'On host {backend} the following links were found not connected: {",".join(errors)}'
+		assert not errors, f'On host {backend} the following links were found not connected: {",".join(errors)}'
